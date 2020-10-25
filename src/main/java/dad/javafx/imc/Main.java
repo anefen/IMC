853af5fab7c871dd2e1f5748f4d21fa17cbcb9cd
AndreaFortes <54024740+AndreaFortes@users.noreply.github.com>
2020-10-25 20:08:30 +0000

    Primer commit

---
+++ src/main/java/dad/javafx/imc/Main.java
@@ -0,0 +1,11 @@
+package dad.javafx.imc;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(IMC.class,args);
+	}
+
+}
